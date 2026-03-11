b4fcbec248dfb38fda3f3b2618c3d34b4c0d7c38
Sumanth Aitham <saitham@clarku.edu>
2026-03-11 16:16:46 -0400

    modified tables

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,41 @@
 import React from 'react'
+import Image from 'next/image'
+import DataTable from './components/DataTable'
 
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {
+    header: 'Name',
+    cellClassName: 'name-cell',
+    cell: (coin) => {
+      const item = coin.item;
+
+    }
+  }
+]
 const page = () => {
   return <main className="main-container">
     <section className="home-grid">
       {/*-------------Coin Overview-------------*/}
       <div id="coin-overview">
         <div className="header">
+          <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={56} height={56} />
+          <div className="coin-info">
+            <div className="coin-name">
+              <p>Bitcoin / BTC</p>
+              <p>$68,981.25</p>
+            </div>
+
+          </div>
 
         </div>
       </div>
       <p>Trending Crypto Coins</p>
+
     </section>
 
     <section className="w-full mt-7 space-y-4">
       <p>Categories</p>
+      <DataTable data={[]} columns={[{ header: 'Title' }, { header: 'Price' }]} />
     </section>
   </main>
 }
